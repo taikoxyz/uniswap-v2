a6fbc612df855ab97c5ca0fe56385d3bdbc50c3a
Keszey Dániel <keszeyd@MacBook-Pro.local>
2025-02-17 16:13:41 +0100

    typo

--- src/gwyneth/GwynethData.sol
+++ src/gwyneth/GwynethData.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 /// @title GwynethData
 /// @notice This library defines various data structures used in the Gwyneth
